4b1ee5c1c08220a6685d0bb4a4cb96fd7916d500
raum <34570224+raumm@users.noreply.github.com>
2024-03-08 09:41:40 +0300

    Initial commit

---
+++ src/main/java/net/raum/firstmod/FirstMod.java
@@ -0,0 +1,17 @@
+package net.raum.firstmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FirstMod implements ModInitializer {
+	public static final String MOD_ID = "firstmod";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		//LOGGER.info("Hello Fabric world!");
+		System.out.println("fhdsuhgudifhg");
+	}
+}
