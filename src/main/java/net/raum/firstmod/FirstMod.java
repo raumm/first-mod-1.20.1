10cc0c46f4e92374eea550906adf3b7954aab704
raum <34570224+raumm@users.noreply.github.com>
2024-03-09 22:21:42 +0300

    added test item

--- src/main/java/net/raum/firstmod/FirstMod.java
+++ src/main/java/net/raum/firstmod/FirstMod.java
@@ -2,6 +2,7 @@ package net.raum.firstmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.raum.firstmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,7 +12,7 @@ public class FirstMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		//LOGGER.info("Hello Fabric world!");
-		System.out.println("fhdsuhgudifhg");
+		LOGGER.info("Hello Fabric world!");
+		ModItems.registerModItems();
 	}
 }
---
+++ src/main/java/net/raum/firstmod/item/ModItems.java
@@ -0,0 +1,29 @@
+package net.raum.firstmod.item;
+
+import net.fabricmc.fabric.api.item.v1.FabricItem;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import net.raum.firstmod.FirstMod;
+
+public class ModItems {
+    public static final Item TESTITEM = registerItem("testitem", new Item(new FabricItemSettings()));
+
+    private static void addItemsToIngredientItemGroup(FabricItemGroupEntries entries) {
+        entries.add(TESTITEM);
+    }
+
+    private static Item registerItem(String name, Item item){
+        return Registry.register(Registries.ITEM, new Identifier(FirstMod.MOD_ID, name), item);
+    }
+    public static void registerModItems() {
+        FirstMod.LOGGER.info("Registering Mod Items for " + FirstMod.MOD_ID);
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(ModItems::addItemsToIngredientItemGroup);
+    }
+}
