7f76bf4d44b6bae00948fd4d8dabd506200bba76
raum <34570224+raumm@users.noreply.github.com>
2024-03-19 21:24:14 +0300

    test
    test

--- src/main/java/net/raum/firstmod/item/ModItems.java
+++ src/main/java/net/raum/firstmod/item/ModItems.java
@@ -1,6 +1,5 @@
 package net.raum.firstmod.item;
 
-import net.fabricmc.fabric.api.item.v1.FabricItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
